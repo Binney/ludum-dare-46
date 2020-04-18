6e487d8d406d6b424eb6fb7ca9786371a0a6d0d6
Sarah Binney <sarah.binney@softwire.com>
2020-04-18 11:49:20 +0100

    add test wav

--- Player.gd
+++ Player.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 export var speed = 400
 export var moving = false
-var screen_size
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,7 +10,7 @@ var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
+
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

c7f4b754e4537516193154fdaf20c854fec75120
Sarah Binney <sarah.binney@softwire.com>
2020-04-19 23:02:38 +0100

    Add custom font for menu text

--- project/GameOver.gd
+++ project/GameOver.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Label.text = 'Level complete. You scored %d percent' % get_tree().get_root().get_child(0).last_level_score
+	$Label.text = 'Level complete! You scored %d percent' % get_tree().get_root().get_child(0).last_level_score
 
 
 func _on_Menu_Btn_pressed():

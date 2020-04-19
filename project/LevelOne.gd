6691279e2ce5f23793c088639eb066a984d5425e
hgcummings <harry@xlr3.com>
2020-04-19 12:50:18 +0100

    Progress and score

--- project/LevelOne.gd
+++ project/LevelOne.gd
@@ -10,7 +10,18 @@ func _ready():
 		if (child is Mousician):
 			child.play_loop()
 
+var sad_time = 0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$ProgressBar.value += delta
+
+	if ($ProgressBar.value == $ProgressBar.max_value):
+		get_tree().get_root().get_child(0).last_level_score = 100 * (($ProgressBar.max_value - sad_time) / $ProgressBar.max_value)
+		get_tree().change_scene('res://GameOver.tscn')
+	
 	var scroll = Vector2(WALKING_SPEED, 0);
 	$CountrysideBackground.scroll_offset -= scroll
+	for child in get_children():
+		if (child is Mousician && child.is_sad()):
+			sad_time += delta

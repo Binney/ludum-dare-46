dbed4382ce48b9aa7392b764d11267e8a19f5208
hgcummings <harry@xlr3.com>
2020-04-19 17:01:36 +0100

    Let's do the time warp again
    (Re-introduce time offsetting)

--- project/Mousician.gd
+++ project/Mousician.gd
@@ -11,6 +11,7 @@ var rng = RandomNumberGenerator.new()
 var pitch_offset = 0
 var time_offset = 0
 var touch_start
+var track_start_time
 
 func _ready():
 	rng.randomize()
@@ -19,6 +20,9 @@ func _ready():
 func _process(delta):
 	if ((!is_sad()) && (rng.randf() < (SADNESS_CHANCE_PER_SECOND * delta))):
 		become_sad()
+		
+	if (is_percussion && time_offset != 0):
+		$AudioStreamPlayer.pitch_scale += (time_offset * delta / 50)
 
 func is_open_menu_event(event):
 	return event is InputEventScreenTouch && event.is_pressed()
@@ -33,14 +37,15 @@ func is_sad():
 
 func play_loop():
 	$AudioStreamPlayer.stream = loop_track
+	track_start_time = OS.get_ticks_usec()
 	$AudioStreamPlayer.play()
 
 func become_sad():
 	match (rng.randi_range(0, 1 if is_percussion else 3)):
 		0:
-			pitch_offset = 1
+			time_offset = 1
 		1:
-			pitch_offset = -1
+			time_offset = -1
 		2:
 			pitch_offset = 1
 		3:
@@ -51,23 +56,43 @@ func update_output():
 	match [pitch_offset, time_offset]:
 		[0, 0]:
 			$AnimatedSprite.play("happy_" + instrument)
+			$AudioStreamPlayer.pitch_scale = 1
+			resync_track()
 			$AudioStreamPlayer.bus = "Healthy"
 		[-1, 0]:
 			$AnimatedSprite.play("sad_" + instrument)
+			$AudioStreamPlayer.pitch_scale = 1
+			resync_track()
 			$AudioStreamPlayer.bus = "Flat"
 		[1, 0]:
 			$AnimatedSprite.play("sad_" + instrument)
+			$AudioStreamPlayer.pitch_scale = 1
+			resync_track()
 			$AudioStreamPlayer.bus = "Sharp"
-		[0, 0]:
-			$AnimatedSprite.play("happy_" + instrument)
-			$AudioStreamPlayer.bus = "Healthy"
 		[0, -1]:
 			$AnimatedSprite.play("sad_" + instrument)
-			$AudioStreamPlayer.bus = "Slow"
+			if (is_percussion):
+				$AudioStreamPlayer.pitch_scale = 1
+				resync_track()
+				$AudioStreamPlayer.bus = "Healthy"
+			else:
+				$AudioStreamPlayer.pitch_scale = pow(2, -1/float(12))
+				$AudioStreamPlayer.bus = "Sharp"
 		[0, 1]:
 			$AnimatedSprite.play("sad_" + instrument)
-			$AudioStreamPlayer.bus = "Fast"
+			if (is_percussion):
+				$AudioStreamPlayer.pitch_scale = 1
+				resync_track()
+				$AudioStreamPlayer.bus = "Healthy"
+			else:
+				$AudioStreamPlayer.pitch_scale = pow(2, 1/float(12))
+				$AudioStreamPlayer.bus = "Flat"
 
+func resync_track():
+	var elapsedMicroseconds = OS.get_ticks_usec() - track_start_time
+	var elapsedSeconds = elapsedMicroseconds / float(1000000)
+	var loopOffset = fmod(elapsedSeconds, $AudioStreamPlayer.stream.get_length())
+	$AudioStreamPlayer.seek(loopOffset)
 
 func _on_Sharpen_performAction():
 	if (pitch_offset < 0):
@@ -88,6 +113,21 @@ func show_menu(position):
 func hide_menu():
 	$ActionMenu.visible = false
 
-
 func _on_ActionMenu_cancel():
+	hide_menu()
+
+func _on_Push_performAction():
+	if (time_offset < 0):
+		time_offset = 0
+	elif (time_offset == 0):
+		time_offset = 1
+	update_output()
+	hide_menu()
+
+func _on_Pull_performAction():
+	if (time_offset > 0):
+		time_offset = 0
+	elif (time_offset == 0):
+		time_offset = -1
+	update_output()
 	hide_menu()

9e38d2b848d97ef2388eb5ad44ea29c186d81583
Sarah Binney <sarah.binney@softwire.com>
2020-04-19 11:55:31 +0100

    comment out random tree debug printing

--- project/ParallaxBackground.gd
+++ project/ParallaxBackground.gd
@@ -10,24 +10,27 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var tree_spawn = rng.randf()
-	if (tree_spawn<0.01):
-		print("Spawned a tree!")
-		$TreesClosest.add_child(new_random_tree())
-		pass
-	elif (tree_spawn<0.02):
-		pass # spawn tree on trees_middle
-	elif (tree_spawn<0.03):
-		pass # spawn tree on trees_far
+	# qq reinstate me
+#	var tree_spawn = rng.randf()
+#	if (tree_spawn<0.01):
+#		$TreesClosest.add_child(new_random_tree())
+#	elif (tree_spawn<0.02):
+#		$TreesMiddle.add_child(new_random_tree())
+#	elif (tree_spawn<0.03):
+#		$TreesFar.add_child(new_random_tree())
 	pass
 
-func new_random_tree():
-	var image = Image.new()
-	image.load("background/tree0") ## qq randomise
-	var texture = ImageTexture.new()
-	texture.create_from_image(image)
-	var sprite = Sprite.new()
-	sprite.texture = texture
-	sprite.centered = false
-	sprite.position = Vector2(35.423, 150) ## qq randomise
-	return sprite
+#func new_random_tree():
+#	var image = Image.new()
+#	image.load("background/tree" + str(rng.randi_range(0, 4)) + ".png")
+#	var texture = ImageTexture.new()
+#	texture.create_from_image(image)
+#	var sprite = Sprite.new()
+#	sprite.texture = texture
+#	sprite.centered = false
+#	sprite.position = Vector2(35.423, 150) ## qq randomise
+#	return sprite
+
+#func recycle_old_trees():
+	# qq need to manually remove any trees that have gone off the left hand side completely
+#	pass

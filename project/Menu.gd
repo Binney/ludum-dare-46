597b61be64b4dccffa9b3724af5eea8a72f70024
Sarah Binney <sarah.binney@softwire.com>
2020-04-20 16:17:41 +0100

    tweak menu mouse

--- project/Menu.gd
+++ project/Menu.gd
@@ -7,14 +7,18 @@ var rng = RandomNumberGenerator.new()
 
 func _ready():
 	player_state = get_tree().get_root().get_child(0)
+	randomise_mouse()
 
 func _process(delta):
 	$MenuMouse.position.x += MENU_MOUSE_SPEED * delta
 
 func _on_VisibilityNotifier2D_screen_exited():
+	$MenuMouse.position.x = -160
+	randomise_mouse()
+
+func randomise_mouse():
 	var random_animation = ANIMATION_NAMES[rng.randi_range(0, 4)]
 	$MenuMouse/AnimatedSprite.frames = load("res://sprite/" + random_animation + ".tres")
-	$MenuMouse.position.x = -160
 
 func _on_Level1_button_pressed():
 	player_state.set_level(1)
